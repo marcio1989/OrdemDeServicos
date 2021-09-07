90f164fa982ec463439a84f771cfa1865b836c9d
marcio1989 <marcio.ricardo.muniz@hotmail.com>
2021-09-07 11:16:28 -0300

    Implementando o pacote DTO

--- src/main/java/com/marcio/os/resources/TecnicoResource.java
+++ src/main/java/com/marcio/os/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marcio.os.domain.Tecnico;
+import com.marcio.os.dtos.TecnicoDTO;
 import com.marcio.os.services.TecnicoService;
 
 @RestController
@@ -18,8 +19,8 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
-		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
+		TecnicoDTO objDTO = new TecnicoDTO(service.findById(id));
+		return ResponseEntity.ok().body(objDTO);
 	}
 }
